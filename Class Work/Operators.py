b690c6332da197e35e0267c24157abb61c6bf798
Vishnuja2003 <vishnujakolla.chowdary@gmail.com>
2025-07-08 20:54:30 +0530

    Operators Programs

---
+++ Class Work/Operators.py
@@ -0,0 +1,7 @@
+#4 Assignment Operator
+a = 10
+a += 10
+#5 Membership Operator
+fruits = ["apple" , "Banana" , "cherry"]
+print("apple" in fruits)  # output : True
+print("Dragon" not in fruits)  #output : True
